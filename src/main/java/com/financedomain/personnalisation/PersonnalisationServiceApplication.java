91e57efb6fbfe7e0225cc3e7c6a1664e376f6963
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-06-29 12:17:37 +0000

    Initial commit

---
+++ src/main/java/com/financedomain/personnalisation/PersonnalisationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.financedomain.personnalisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnalisationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnalisationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financedomain/personnalisation/PersonnalisationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financedomain.personnalisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnalisationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
