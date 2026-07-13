5beae2f98c2905d48131e84d2721fc2143d9a18b
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-13 16:26:49 +0000

    feat: set up circuit breaker

--- src/main/java/com/financedomain/personnalisation/proxy/PythonPersonalizationProxy.java
+++ src/main/java/com/financedomain/personnalisation/proxy/PythonPersonalizationProxy.java
@@ -1,10 +1,11 @@
 package com.financedomain.personnalisation.proxy;
 
+import com.financedomain.personnalisation.proxy.fallback.PythonPersonalizationProxyFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "python-personalization-api", url = "${python.api.url}")
+@FeignClient(name = "python-personalization-api", url = "${python.api.url}", fallback = PythonPersonalizationProxyFallback.class)
 public interface PythonPersonalizationProxy {
 
     @GetMapping("/api/v1/usages/{msisdn}")
