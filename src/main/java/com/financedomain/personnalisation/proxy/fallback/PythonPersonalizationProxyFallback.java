5087b7777a3a1d623cdf688b187b6a63a5e52a8f
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-25 18:18:29 +0000

    fix: clean code

--- src/main/java/com/financedomain/personnalisation/proxy/fallback/PythonPersonalizationProxyFallback.java
+++ src/main/java/com/financedomain/personnalisation/proxy/fallback/PythonPersonalizationProxyFallback.java
@@ -10,7 +10,7 @@ public class PythonPersonalizationProxyFallback implements PythonPersonalization
     @Override
     public Object getClientUsages(String msisdn) {
         System.err.println("[Fallback] L'API Python de personnalisation est indisponible. Retour d'une liste vide par défaut pour le msisdn : " + msisdn);
-        // Dégradation gracieuse : retourne une liste vide pour ne pas faire planter l'application cliente
+
         return new ArrayList<>();
     }
 }
